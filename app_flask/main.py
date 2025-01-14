7238cf08ca86cd54f87b62b8fb8ed3faa208cf48
MurrayChapman17 <murraychapman17@gmail.com>
2025-01-14 12:18:55 +0000

    adding multiple-collections flask capability

--- app_flask/main.py
+++ app_flask/main.py
@@ -7,44 +7,66 @@ from NGD_API_Wrappers import *
 app = Flask(__name__)
 
 from marshmallow import Schema, INCLUDE
-from marshmallow.fields import Integer, String, Boolean
+from marshmallow.fields import Integer, String, Boolean, List
 
-class ItemsAuthSchema(Schema):
+class BaseSchema(Schema):
     class Meta:
         unknown = INCLUDE  # Allows additional fields to pass through to query_params
 
-class ItemsAuthLimitSchema(ItemsAuthSchema):
+class LimitSchema(BaseSchema):
     limit = Integer(data_key='limit', required=False)
     request_limit = Integer(data_key='request-limit', required=False)
     verbose = Boolean(data_key='verbose', required=False)
 
-class ItemsAuthGeomSchema(ItemsAuthSchema):
-    wkt = String(required=False)
+class GeomSchema(BaseSchema):
+    wkt = String(required=False, data_key='wkt')
 
-class ItemsAuthLimitGeomSchema(ItemsAuthLimitSchema, ItemsAuthGeomSchema):
-    """Combining the two super classes"""
+class ColSchema(BaseSchema):
+    collection = List(String(), required=True, data_key='collection')
+
+class LimitGeomSchema(LimitSchema, GeomSchema):
+    """Combining Limit and Geom schemas"""
+
+class LimitColSchema(LimitSchema, ColSchema):
+    """Combining Limit and Col schemas"""
+
+class GeomColSchema(GeomSchema, ColSchema):
+    """Combining Geom and Col schemas"""
+
+class LimitGeomColSchema(LimitSchema, GeomSchema, ColSchema):
+    """Combining Limit, Geom, and Col schemas"""
 
 @app.route("/")
 def hello_world():
     args = request.args
     return args
 
-def create_item_route(schema_class: Schema, handler_function, route_suffix: str):
-    """Factory function to create route handlers for item endpoints"""
-    route = f"/catalyst/features/ngd/ofa/v1/collections/<collection>/items/{route_suffix}"
+def delistify(params: dict):
+    for k, v in params.items():
+        if k != 'collection':
+            params[k] = v[0]
 
-    def handler(collection: str):
+def create_item_route(schema_class: Schema, handler_function, route_suffix: str, multi_collections: bool = False):
+    """Factory function to create route handlers for item endpoints"""
+
+    collection_path_component = 'multi-collection' if multi_collections else '<collection>'
+    route = f"/catalyst/features/ngd/ofa/v1/collections/{collection_path_component}/items/{route_suffix}"
+
+    def handler(collection: str | list = None):
         schema = schema_class()
-        parsed_params = schema.load(request.args)
+        args = request.args.to_dict(flat=False)
+        delistify(args)
+        parsed_params = schema.load(args)
         
         custom_params = {
             k: parsed_params.pop(k)
             for k in schema.fields.keys()
             if k in parsed_params
         }
-        
+        if collection:
+            custom_params['collection'] = collection
+
         return handler_function(
-            collection=collection,
             query_params=parsed_params,
             **custom_params
         )
@@ -61,27 +83,55 @@ def create_item_route(schema_class: Schema, handler_function, route_suffix: str)
     return handler
 
 items_auth_handler = create_item_route(
-    ItemsAuthSchema,
+    BaseSchema,
     items_auth,
     "items-auth"
 )
 
 items_auth_limit_handler = create_item_route(
-    ItemsAuthLimitSchema,
+    LimitSchema,
     items_auth_limit,
     "items-auth-limit"
 )
 
 items_auth_geom_handler = create_item_route(
-    ItemsAuthGeomSchema,
+    GeomSchema,
     items_auth_geom,
     "items-auth-geom"
 )
 
 items_auth_limit_geom_handler = create_item_route(
-    ItemsAuthLimitGeomSchema,
+    LimitGeomSchema,
     items_auth_limit_geom,
     "items-auth-limit-geom"
+)
+
+items_auth_col_handler = create_item_route(
+    ColSchema,
+    items_auth_col,
+    "items-auth-col",
+    True
+)
+
+items_auth_limit_col_handler = create_item_route(
+    LimitColSchema,
+    items_auth_limit_col,
+    "items-auth-limit-col",
+    True
+)
+
+items_auth_geom_col_handler = create_item_route(
+    GeomColSchema,
+    items_auth_geom_col,
+    "items-auth-geom-col",
+    True
+)
+
+items_auth_limit_geom_col_handler = create_item_route(
+    LimitGeomColSchema,
+    items_auth_limit_geom_col,
+    "items-auth-limit-geom-col",
+    True
 )
 
 if __name__ == "__main__":
