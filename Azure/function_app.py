6abbb30777d97a7002a452934556bb2f169d81d8
MurrayChapman17 <murraychapman17@gmail.com>
2025-05-09 16:10:53 +0100

    SpanProcessor

--- Azure/function_app.py
+++ Azure/function_app.py
@@ -3,12 +3,24 @@ from azure.functions import HttpRequest, HttpResponse
 from NGD_API_Wrappers import *
 import json
 
+from opentelemetry.sdk.trace import SpanProcessor
+
+class SpanEnrichingProcessor(SpanProcessor):
+
+    def on_end(self, span):
+        # Prefix the span name with the string "Updated-".
+        span._name = "Updated-" + span.name
+        # Add the custom dimension "CustomDimension1" with the value "Value1".
+        span._attributes["CustomDimension1"] = "Value1"
+         # Add the custom dimension "CustomDimension2" with the value "Value2".
+        span._attributes["CustomDimension2"] = "Value2"
+
 # Import the necessary packages.
 from azure.monitor.opentelemetry import configure_azure_monitor
 from opentelemetry import traced
 
 # # Create a SpanEnrichingProcessor instance.
-span_enrich_processor = SpanEnrichingProcessor()
+# span_enrich_processor = SpanEnrichingProcessor()
 
 # # Configure OpenTelemetry to use Azure Monitor with the specified connection string.
 # # Replace `<your-connection-string>` with the connection string to your Azure Monitor Application Insights resource.
