79f46a6e6f8bd7a229c41dd47b623be2ac5d27c7
MurrayChapman17 <murraychapman17@gmail.com>
2025-08-07 11:04:33 +0100

    correcting func names

--- src/catalyst_ngd_wrappers/__init__.py
+++ src/catalyst_ngd_wrappers/__init__.py
@@ -8,9 +8,9 @@ from .ngd_api_wrappers import (
     items_geom,
     items_col,
     items_limit_geom,
-    items_col_geom,
-    items_col_limit,
-    items_col_limit_geom
+    items_limit_col,
+    items_geom_col,
+    items_limit_geom_col
 )
 
 __all__ = [
